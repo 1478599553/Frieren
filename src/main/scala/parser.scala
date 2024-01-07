9c21b187c9d919450d5b2ad9e4c662bf2c342134
Draming <1478599553@qq.com>
2024-01-07 13:56:30 +0800

    test

--- src/main/scala/parser.scala
+++ src/main/scala/parser.scala
@@ -4,11 +4,11 @@ import scala.util.parsing.combinator._
 
 sealed trait AstNode
 case class Symbol(name: String) extends AstNode
-case class Number(value: Integer) extends AstNode
+case class Number(value: Int) extends AstNode
 case class Add(value1: AstNode, value2: AstNode) extends AstNode
 case class Abstraction(param: List[Symbol], body: List[AstNode]) extends AstNode
 case class Apply(func: AstNode, arg: List[AstNode]) extends AstNode
-
+case class Let()
 
 object LispParser extends RegexParsers {
     
@@ -37,33 +37,3 @@ object LispParser extends RegexParsers {
         case _ => throw new IllegalArgumentException("Parsing failed")
     }
 }
-
-object Test extends App {
-    val caseList = List(
-        "((lambda x (x 1 2)) (lambda x x))",
-        "(lambda x (lambda f (lambda g (g (f x) (f x x)))))",
-        "(lambda x ((lambda (f x) (f x)) x))",
-        "(lambda (f x a) (f x (x a)))",
-        "(lambda (a b c d e) ((lambda (a b f g h) (a b c d e f g h))))",
-        "(lambda x (+ x 1))",
-        "((lambda x (x x)) (lambda x (x x)))",
-        "(+ 1 ((lambda x (+ x 2)) 3))",
-        "((lambda (f g x) (f x (g x))) (lambda (x y) x) (lambda (x y) x))",
-        "((lambda f (f (lambda (x y) x) (lambda (x y) x))) (lambda (x y) ((x (lambda x x) 1) (y 2 3))))",
-        "(lambda (f g x) (f x (g x)))",
-        "((lambda (f g x) (f x (g x))) ((lambda (x y) x) ((lambda (f g x) (f x (g x))) ((lambda (f g x) (f x (g x))) (lambda (x y) x) (lambda (x y) x)))) (lambda (x y) x))",
-        "((lambda (s1 k1 s2 i k2) (s1 (k1 (s2 i)) k2)) (lambda (f g x) (f x (g x))) (lambda (x y) x) (lambda (f g x) (f x (g x))) ((lambda (f g x) (f x (g x))) (lambda (x y) x) (lambda (x y) x)) (lambda (x y) x))",
-        "(lambda x (lambda y y))",
-        "(lambda a (lambda b (lambda f (f (a b)))))",//λ a. λ b. λ f. f a b
-        "(lambda x1 (x1 x1))",
-
-
-    )
-    
-    caseList.foreach(it =>
-        println(s"$it => ${LispParser.parseToAst(it)}")
-        println(s"${infer(LispParser.parseToAst(it))}")
-    )
-}
-
-
---
+++ src/main/scala/test.scala
@@ -0,0 +1,38 @@
+package frieren
+
+private val caseList = List(
+    "((lambda x (x 1 2)) (lambda x x))",
+    "(lambda x (lambda f (lambda g (g (f x) (f x x)))))",
+    "(lambda x ((lambda (f x) (f x)) x))",
+    "(lambda (f x a) (f x (x a)))",
+    "(lambda (a b c d e) ((lambda (a b f g h) (a b c d e f g h))))",
+    "(lambda x (+ x 1))",
+    "((lambda x (x x)) (lambda x (x x)))",
+    "(+ 1 ((lambda x (+ x 2)) 3))",
+    "((lambda (f g x) (f x (g x))) (lambda (x y) x) (lambda (x y) x))",
+    "((lambda f (f (lambda (x y) x) (lambda (x y) x))) (lambda (x y) ((x (lambda x x) 1) (y 2 3))))",
+    "(lambda (f g x) (f x (g x)))",
+    "((lambda (f g x) (f x (g x))) ((lambda (x y) x) ((lambda (f g x) (f x (g x))) ((lambda (f g x) (f x (g x))) (lambda (x y) x) (lambda (x y) x)))) (lambda (x y) x))",
+    "((lambda (s1 k1 s2 i k2) (s1 (k1 (s2 i)) k2)) (lambda (f g x) (f x (g x))) (lambda (x y) x) (lambda (f g x) (f x (g x))) ((lambda (f g x) (f x (g x))) (lambda (x y) x) (lambda (x y) x)) (lambda (x y) x))",
+    "(lambda x (lambda y y))",
+    "(lambda a (lambda b (lambda f (f (a b)))))", //λ a. λ b. λ f. f a b
+    "(lambda x1 (x1 x1))",
+    "((lambda x x) 1)" +
+        "((lambda x x) (lambda x x))"
+
+)
+object test extends App {
+    testParser()
+    testInfer()
+}
+
+def testParser(): Unit = {
+    caseList.foreach(it =>
+        println(s"$it => ${LispParser.parseToAst(it)}")
+    )
+}
+def testInfer(): Unit = {
+    caseList.foreach(it =>
+        println(s"${infer(LispParser.parseToAst(it))}")
+    )
+}
