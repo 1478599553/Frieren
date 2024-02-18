77e5512e06bb4ad593c036850433404646d7cb77
togetherfrist <709798511@qq.com>
2024-02-18 21:38:47 +0800

    fix11

--- src/main/scala/parser.scala
+++ src/main/scala/parser.scala
@@ -2,7 +2,7 @@ package frieren
 
 import scala.util.parsing.combinator.*
 import frieren.*
-import _root_.frieren.Pattern.{ConstructorDestruction, Identifier, WildCard}
+import _root_.frieren.Pattern.*
 
 object FrierenParser extends RegexParsers {
     def debug[T](p: Parser[T]): Parser[T] = p ^^{it =>
@@ -91,7 +91,7 @@ object FrierenParser extends RegexParsers {
     def matchexpr : Parser[Match] = (spaced("match") ~> spaced(symbol) <~ (spaced("with") | exception("match without with"))) ~ rep(spaced(pattern) ~ spaced(expr) ^^ {case p ~ e => (p, e)}) ^^ {case obj ~ arms => Match(obj, arms)}
 
     def pattern : Parser[Pattern] = spaced("|") ~> spaced("_") <~ (spaced("->") | exception("pattern without ->")) ^^ (_ => WildCard) |
-        spaced("|") ~> spaced("""([a-zA-Z_][a-zA-Z_1-9]*)""".r) ~ bracket(repsep(spaced("""([a-zA-Z_][a-zA-Z_1-9]*)""".r), spaced(","))) <~ (spaced("->") | exception("pattern without ->")) ^^ {case constructor ~ items => ConstructorDeconstruction(constructor, items)} |
+        spaced("|") ~> spaced("""([a-zA-Z_][a-zA-Z_1-9]*)""".r) ~ bracket(repsep(spaced(pattern), spaced(","))) <~ (spaced("->") | exception("pattern without ->")) ^^ {case constructor ~ items => ConstructorDeconstruction(constructor, items)} |
         spaced("|") ~> spaced("""([a-zA-Z_][a-zA-Z_1-9]*)""".r) <~ (spaced("->") | exception("pattern without ->")) ^^ (s => Identifier(s))
     def exception(message: String): Parser[AstNode] = {
         throw ParserException(message)
