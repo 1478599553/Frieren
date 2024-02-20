84e0e069a3479f4dd832d6b6823437809988c5f4
Draming <1478599553@qq.com>
2024-02-20 21:08:35 +0800

    ready to roll

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -19,9 +19,9 @@ def repl(): Unit = {
     try {
       val ast = FrierenParser.parseToAst(input)
       //println(s"parsed to: $ast")
-      //println(s"type inferred to: \n${infer(ast)}")
+      println(s"type inferred to: \n${infer(ast)}")
       //println(s"interpreted to: ${interp(ast, Map[String, Value]())}")
-      println(s"compiled to: ${compile(ast)}")
+      //println(s"compiled to: ${compile(ast)}")
     } catch {
       case e: Throwable =>
         println(s"Error: ${e.getMessage}")
